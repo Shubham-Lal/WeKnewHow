2e35dc1993e6bb76488de981e1b680dee991fd80
CERTIFIED2003 <lal.shubham027@gmail.com>
2023-05-06 21:31:45 +0530

    Extended the countdown period

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -1,7 +1,5 @@
 function makeTimer() {
-
-	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-	var endTime = new Date("30 April 2023 12:00:00 GMT+01:00");
+	var endTime = new Date("01 June 2023 12:00:00 GMT+01:00");
 	endTime = (Date.parse(endTime) / 1000);
 
 	var now = new Date();
